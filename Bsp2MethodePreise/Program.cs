6934f30a5e51516a7c3863d4d13612addfd6118f
Steffi <steffiklaic@gmx.at>
2023-05-22 11:56:00 +0200

    Initial commit

---
+++ Bsp2MethodePreise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection.Emit;
+using System;
+using System.Globalization;
+
+namespace Bsp2MethodePreise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Eine Methode schreiben, die die angegebene Werte wie folgt(untereinander) ausgibt:
+            // Produktname: USB - C Kabel
+            // Anzahl: 16
+
+            // Einzelpreis: 12.90
+            // Bitte passende Datentypen auswählen.
+
+
+
+
+            // Hinweis: Formatierungsoption $"{0:F2}" gerundet auf zwei Dezimalstellen.
+
+            string produkt = "USB - C Kabel";
+            int anzahl = 16;
+            double preis = 12.90;
+
+            PreisAusgeben(produkt, anzahl, preis);
+        }
+
+        public static void PreisAusgeben(string produkt, int anzahl, double preis)
+        {
+            Console.WriteLine("Produktname: " + produkt);
+            Console.WriteLine("Anzahl: " + anzahl);
+            Console.WriteLine("Einzelpreis: " + "{0:F2}", preis);
+        }
+    }
+}
